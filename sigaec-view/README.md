f4351935884487a5827af383f98fbc2937a85ebb
unknown <gusbra21@outlook.com>
2022-04-29 20:08:24 -0300

    sincronizando

---
+++ sigaec-view/README.md
@@ -0,0 +1,15 @@
+# SIGAEC-VIEW
+
+Para executar este projeto, é necessário ter o módulo @vue/cli.
+Para o desenvolvimento da interface está sendo utilizado o plugin BootstrapVue.
+
+## Executar o projeto para desenvolvimento com hot-reload
+```
+npm run serve
+```
+
+### Compilar para implantação (deploy) em servidor
+```
+npm run build
+```
+O código resultante irá ser gerado na pasta /dist, dentro da raiz do projeto sigaec-view. O conteúdo dessa pasta é que deve ser implantando em um servidor web estático.
---
+++ sigaec-view/src/views/TrabalhosAcademicos.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="trabalhos-academicos">
+    <h1>Trabalhos Acadêmicos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrabalhosAcademicos",
+  data() {
+    return {
+      user: {
+        name: "user"
+      }
+    };
+  },
+  methods: {
+    getUserData() {
+      this.$http
+        .get("/api/user")
+        .then(response => {
+          console.log(response);
+          this.$set(this, "user", response.data.user);
+        })
+        .catch(errors => {
+          console.log(errors);
+          this.$router.push("/");
+        });
+    }
+  },
+  mounted() {
+    this.getUserData();
+  }
+};
+</script>
+
+<style></style>

cd074d6abbda9912b8c27581fd250c374ffca93a
unknown <gustavobrandao31@outlook.com>
2022-04-29 20:08:24 -0300

    sincronizando

---
+++ sigaec-view/src/views/EventosInscritos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="eventos-incritos">
+    <h1>Eventos inscritos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventosInscritos"
+};
+</script>
+
+<style></style>

7a663b963b874ac22bd5d0f028982f6819342b97
Gustavo <gustavobrandao31@outlook.com>
2022-05-26 20:53:10 -0300

    Ajustes no css e responsividade 1.0

--- app-api/src/api/repositories/database/database.js
+++ app-api/src/api/repositories/database/database.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname+'../../../../../config/config.json'))[env];
+const config = require(path.join(__dirname +'../../../../../config/config.json'))[env];
 const db = {};
 const basename = path.basename(__filename);
 
 let sequelize;
-if (config.use_env_variable) {
+if (config.use_env_variable)
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+else
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
 
 fs
   .readdirSync(__dirname)
@@ -33,7 +33,5 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-
 
 module.exports = db;

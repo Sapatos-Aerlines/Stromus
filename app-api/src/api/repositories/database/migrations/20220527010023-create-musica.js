7d0e9204262e6fbbfb4944281fa94f83f7532115
Warzinhuw <rafael_martorano@hotmail.com>
2022-06-09 22:45:26 -0300

    Arrumado referências das chaves estrangeiras

--- app-api/src/api/repositories/database/migrations/20220527010023-create-musica.js
+++ app-api/src/api/repositories/database/migrations/20220527010023-create-musica.js
@@ -26,8 +26,7 @@ module.exports = {
         onUpdate: 'CASCADE',
         references: {
           model: 'artistas',
-          key: 'id',
-          as: 'idArtista',
+          key: 'id'
         }
       },
       idAlbum: {
@@ -36,8 +35,7 @@ module.exports = {
         onUpdate: 'CASCADE',
         references: {
           model: 'albuns',
-          key: 'id',
-          as: 'idAlbum',
+          key: 'id'
         }
       }
     });

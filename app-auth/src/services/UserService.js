174870ffc175b497914598ec19e1c4a348f63026
Gustavo <gustavobrandao31@outlook.com>
2022-06-11 21:45:03 -0300

    Sync de login

---
+++ app-auth/src/services/UserService.js
@@ -0,0 +1,47 @@
+
+require('dotenv').config();
+// This will help us connect to the database
+const dbo = require('../db/conn');
+const {ObjectId} = require("mongodb");
+const collName = 'users';
+
+module.exports = {
+    async create(matchUser, callbackAtlasResponse) {
+        const dbConnect = dbo.getDb();
+
+        await dbConnect
+            .collection(collName)
+            .insertOne(matchUser, callbackAtlasResponse);
+    },
+
+    async deleteByUsername(req, res, next) {
+
+    },
+
+    async update(req, res, next) {
+
+    },
+
+    async getUserById(userId){
+        const dbConnect = dbo.getDb();
+        return await dbConnect
+            .collection(collName)
+            .findOne(
+                {
+                    _id: ObjectId(userId)
+                }
+            );
+    },
+
+    async getByUsername(username) {
+        const dbConnect = dbo.getDb();
+
+        return await dbConnect
+            .collection(collName)
+            .findOne(
+                {
+                    username:username
+                }
+            );
+    },
+}

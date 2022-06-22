63084a7cb6b22b22bab5aa1c3432570c761a50f9
Warzinhuw <rafael_martorano@hotmail.com>
2022-06-22 19:56:29 -0300

    Atualizado state manager

--- app-front/pages/main.vue
+++ app-front/pages/main.vue
@@ -546,14 +546,15 @@
   export default {
 
     //Executado quando a instância do Vue estiver construída
-    async asyncData({ $axios, $router }) {
-      const authToken = typeof window !== 'undefined' ? localStorage.getItem('token') : null // se tiver carregando client side, recupera o token do usuário
+    async asyncData({store, $axios, redirect }) {
+      console.log("STATE: "+store.state.authenticationToken)
+      const authToken = typeof window !== 'undefined' ? store.state.authenticationToken : null // se tiver carregando client side, recupera o token do usuário
 
       // Check if user is logged in.
       if (authToken === null) {
           // This means that there ISN'T JWT and no user is logged in.
-          // $axios.defaults.headers.common.Authorization = null;
-          // $router.push('/');
+           $axios.defaults.headers.common.Authorization = null;
+           redirect('/');
       } else {
           // This means that there IS a JWT so someone must be logged in.
           $axios.defaults.headers.common.Authorization = `Bearer ${authToken}`; // salva o token para usar nos headers nas requisições

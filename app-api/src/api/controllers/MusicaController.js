016a4c0787fcbec3d3de7066b17739fbfb33428d
Gustavo <gusbra21@outlook.com>
2022-06-22 20:50:00 -0300

    Edições 1.0

--- app-api/src/api/controllers/MusicaController.js
+++ app-api/src/api/controllers/MusicaController.js
@@ -33,6 +33,22 @@ module.exports = {
         });
     },
 
+    update: function (req, res){
+        const {idArtista, idAlbum, nome, duracao, estilo} = req.body
+        MusicaService.update(
+            {idArtista, idAlbum, nome, duracao, estilo}
+        ).then((status) => {
+            res.statusCode = 201; // Status HTTP para created;
+            res.set("Content-Type", "application/json");
+            res.send(JSON.stringify(status));
+        }).catch(error => {
+            console.error(error);
+            res.statusCode = 500; //Status HTTP para erro interno
+            res.set("Content-Type", "application/text");
+            res.send(error.message);
+        });
+    },
+    
     // handler para recuperar uma música por id
     getById: function (req, res) {
         const id = req.params.id;
